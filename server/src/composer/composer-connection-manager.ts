b15f742e0f5fe15782b04696b4312c14736e32e9
Peckstadt Yves <peckstadt.yves@gmail.com>
2018-02-27 19:32:34 +0900

    :+1: updated connection profile to hlfv11

--- server/src/composer/composer-connection-manager.ts
+++ server/src/composer/composer-connection-manager.ts
@@ -11,25 +11,61 @@ export default class ComposerConnectionManager {
 
   static BUSINESS_NETWORK = 'cargo-network';
   static CONNECTION_PROFILE = {
-    type: 'hlfv1',
-    name: 'ca.org1.example.com',
-    orderers: [
-      {
-        url: 'grpc://localhost:7050'
+    "name": "hlfv1",
+    "x-type": "hlfv1",
+    "x-commitTimeout": 300,
+    "version": "1.0.0",
+    "client": {
+      "organization": "Org1",
+      "connection": {
+        "timeout": {
+          "peer": {
+            "endorser": "300",
+            "eventHub": "300",
+            "eventReg": "300"
+          },
+          "orderer": "300"
+        }
       }
-    ],
-    peers: [
-      {
-        requestURL: 'grpc://localhost:7051',
-        eventURL: 'grpc://localhost:7053',
-      }
-    ],
-    ca: {
-      url: 'http://localhost:7054'
     },
-    channel: 'composerchannel',
-    mspID: 'Org1MSP',
-    timeout: '300'
+    "channels": {
+      "composerchannel": {
+        "orderers": [
+          "orderer.example.com"
+        ],
+        "peers": {
+          "peer0.org1.example.com": {}
+        }
+      }
+    },
+    "organizations": {
+      "Org1": {
+        "mspid": "Org1MSP",
+        "peers": [
+          "peer0.org1.example.com"
+        ],
+        "certificateAuthorities": [
+          "ca.org1.example.com"
+        ]
+      }
+    },
+    "orderers": {
+      "orderer.example.com": {
+        "url": "grpc://${HOST}:7050"
+      }
+    },
+    "peers": {
+      "peer0.org1.example.com": {
+        "url": "grpc://${HOST}:7051",
+        "eventUrl": "grpc://${HOST}:7053"
+      }
+    },
+    "certificateAuthaorities": {
+      "ca.org1.example.com": {
+        "url": "http://${HOST}:7054",
+        "caName": "ca.org1.example.com"
+      }
+    }
   };
 
   private cardStore: MongooseCardStore;
