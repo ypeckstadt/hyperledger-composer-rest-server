0b0d6561f6b625ce800b1edd2d8e7f4df6a3f73c
Peckstadt Yves <peckstadt.yves@gmail.com>
2018-03-09 10:16:31 +0900

    :+1: dont store composer card as base64 anymore

--- server/src/composer/composer-card.ts
+++ server/src/composer/composer-card.ts
@@ -2,7 +2,12 @@ import * as Mongoose from "mongoose";
 
 export interface IComposerCard extends Mongoose.Document {
   cardName: string;
-  base64: string;
+  connectionProfile: string;
+  businessNetwork: string;
+  enrollmentSecret: string;
+  userName: string;
+  roles: string;
+  version: number;
   createdAt: Date;
   updateAt: Date;
 }
@@ -11,7 +16,12 @@ export interface IComposerCard extends Mongoose.Document {
 export const ComposerCardSchema = new Mongoose.Schema(
   {
     cardName: { type: String, unique: true, required: true },
-    base64: { type: String, unique: true, required: true }
+    connectionProfile: { type: String, unique: false, required: true },
+    businessNetwork: { type: String, unique: false, required: true },
+    enrollmentSecret: { type: String, unique: false, required: true },
+    userName: { type: String, unique: false, required: true },
+    roles: { type: String, unique: false, required: false },
+    version: { type: Number, unique: false, required: true }
   },
   {
     timestamps: true
